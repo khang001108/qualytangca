084fb36a8a951b3a93ce89af27ab43ca332a6201
khang001108 <khang120k120@gmail.com>
2025-11-27 06:32:27 +0700

    a

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,31 @@
 // pages/_app.js
 // Cấu trúc chính của ứng dụng Next.js với các provider toàn cục
 
-
-
 import "../styles/globals.css";
 import "react-datepicker/dist/react-datepicker.css";
 import * as Tooltip from "@radix-ui/react-tooltip";
 import { ToastProvider } from "../components/base/ToastContext";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <Tooltip.Provider>
-      <ToastProvider>
-        <Component {...pageProps} />
-      </ToastProvider>
-    </Tooltip.Provider>
+    <>
+      <Head>
+        {/* Set favicon */}
+        <link rel="icon" href="/favicon (2).ico" />
+
+        {/* Nếu muốn PNG thì bật dòng này */}
+        {/* <link rel="icon" type="image/png" href="/favicon.png" /> */}
+
+        {/* Title mặc định nếu cậu muốn */}
+        <title>Quản Lý Tăng Ca</title>
+      </Head>
+
+      <Tooltip.Provider>
+        <ToastProvider>
+          <Component {...pageProps} />
+        </ToastProvider>
+      </Tooltip.Provider>
+    </>
   );
 }
