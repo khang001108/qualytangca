e2455286f83272a233a19ad723664e7106a5418a
khang001108 <khang120k120@gmail.com>
2026-06-07 12:45:52 +0700

    a

--- next.config.js
+++ next.config.js
@@ -1,8 +1,2 @@
-const withPWA = require("next-pwa")({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
-});
-
-module.exports = withPWA({});
+/** Next.js default config - empty for now */
+module.exports = {};
